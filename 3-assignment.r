dcb7fe361afa96e8b8378a280786d04b15182105
C.M. Gienger <39389399+cmgienger@users.noreply.github.com>
2022-08-31 11:32:18 -0500

    updates

--- 3-assignment.r
+++ 3-assignment.r
@@ -2,41 +2,57 @@
 #R Lecture 3: Practice exercises for subsetting and manipulating data
 
 library(dplyr)
-library(data.table) #might need to install if not on your computer
 
 #First load a dataset to work with;
-
 data(mtcars) # where does this data come from? Is it magic?
 # for reference:  library(help = "datasets")
 
 #fix problem with car names not being in a dataframe column
-mtcar_data <- data.table(mtcars, keep.rownames = TRUE)
-mtcar_data <- rename(mtcar_data, name = rn)
-mtcar_data #shows dataframe with the car names in first column
+mtcars$names <- rownames(mtcars)
+mtcars %>% relocate(names)
 
 ##################  HOMEWORK  #################################
-#1. What are at least 2 different ways to view the 'mtcars' dataset?
+# Assign each answer to a new object (e.g. quest4, quest5, etc.). Try to use good coding practice
+# such as breaking lines after the pipe operator (%>%) to give a sequence of readable steps. 
+
+
+#1. How you you programmatically (i.e. using code) view the 'mtcars' dataset?
+quest_1 <-
 
 #2. How do you determine the different types of data contained in mtcars?
+quest_2 <-
 
 #3. Assign the mtcars dataset to a new object.
-
+quest_3 <-
+  
 # Using our knowledge of dplyr, answer the following:
 # I recommend using 'pipes' where possible (but not required), as they are more intuitive.
-# Assign each answer to a new object (e.g. quest4, quest5, etc.)
-
+  
 #4. What is the mean and standard deviation of horsepower (hp)
 #for each group of cars, as measured by their number of cylinders (cyl)?
-
+quest_4 <-
+  
 #5. Of the cars having 8 cylinders, which six have the most horsepower? (show just the top 6) 
-
-#6. Produce a table of fuel efficiency (mpg) for 4-cylinder cars ranked best to worst.
-
+quest_5 <-
+  
+#6. Produce a table of fuel efficiency (mpg) for 4-cylinder cars ordered best to worst.
+quest_6 <-
+  
 #7. List the cars that have fuel efficiency (mpg) greater than or equal to 21 miles per gallon and 
 # horsepower (hp) greater than 100.
+quest_7 <-  
 
 #8. What are the top 10 cars in ratio of horsepower (hp) to fuel-efficiency (mpg)?
+quest_8 <-   
 
+#9. List all the Merc cars (Mercury) in order of increasing fuel efficiency (mpg).
+  #hint_1 (using str_detect in tidyverse) https://suzan.rbind.io/2018/02/dplyr-tutorial-3/#filtering-rows-based-on-regex
+  #hint_2 (using grepel in base R): https://www.statology.org/filter-rows-that-contain-string-dplyr/
+  
+quest_9 <-  
+
+#10. Give the name, mpg, disp, and wt for the car with the highest horse power (hp).
+quest_10 <-  
 ###############################################################
 #Turn in a properly formatted script file for your answers. I am going to run the 
 #entire script as one chunk of code, and if it doesn't work, I won't grade it. 
---
+++ no_commit_assignment_code/3-assignment_blank.R
@@ -0,0 +1,62 @@
+#C.M. Gienger
+#R Lecture 3: Practice exercises for subsetting and manipulating data
+
+library(dplyr)
+library(data.table) #might need to install if not on your computer
+
+#First load a dataset to work with;
+
+data(mtcars) # where does this data come from? Is it magic?
+# for reference:  library(help = "datasets")
+
+#fix problem with car names not being in a dataframe column
+mtcars <- data.table(mtcars, keep.rownames = TRUE)
+mtcars <- rename(mtcar_data, name = rn)
+mtcars #shows dataframe with the car names in first column
+
+##################  HOMEWORK  #################################
+# Assign each answer to a new object (e.g. quest4, quest5, etc.). Try to use good coding practice
+# such as breaking lines after the pipe operator (%>%) to give a sequence of readable steps. 
+
+
+#1. How you you programmatically (i.e. using code) view the 'mtcars' dataset?
+quest_1 <-
+
+#2. How do you determine the different types of data contained in mtcars?
+quest_2 <-
+
+#3. Assign the mtcars dataset to a new object.
+quest_3 <-
+  
+# Using our knowledge of dplyr, answer the following:
+# I recommend using 'pipes' where possible (but not required), as they are more intuitive.
+  
+#4. What is the mean and standard deviation of horsepower (hp)
+#for each group of cars, as measured by their number of cylinders (cyl)?
+quest_4 <-
+  
+#5. Of the cars having 8 cylinders, which six have the most horsepower? (show just the top 6) 
+quest_5 <-
+  
+#6. Produce a table of fuel efficiency (mpg) for 4-cylinder cars ordered best to worst.
+quest_6 <-
+  
+#7. List the cars that have fuel efficiency (mpg) greater than or equal to 21 miles per gallon and 
+# horsepower (hp) greater than 100.
+quest_7 <-  
+
+#8. What are the top 10 cars in ratio of horsepower (hp) to fuel-efficiency (mpg)?
+quest_8 <-   
+
+#9. List all the Merc cars (Mercury) in order of increasing fuel efficiency (mpg).
+quest_9 <-  
+
+#10. Give the name, mpg, disp, and wt for the car with the highest horse power (hp).
+quest_10 <-  
+###############################################################
+#Turn in a properly formatted script file for your answers. I am going to run the 
+#entire script as one chunk of code, and if it doesn't work, I won't grade it. 
+#You need to make it bullet-proof for your 'collaborator' (me). 
+#Review Beckerman Section 3.3.5 for reference.
+#OK to collaborate with others, but don't let people sponge off your hard work.
+#Due one week from today (before class) via D2L Assignments submission box.
